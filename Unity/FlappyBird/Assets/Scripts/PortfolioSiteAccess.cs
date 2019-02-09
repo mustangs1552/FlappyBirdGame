9e162af08d18506f82d679eb9f46191a3a39e97e
Matthew George <mustangs155222@gmail.com>
2019-02-08 21:55:39 -0500

    Fiinished adding all methods of getting scores from server.

--- Unity/FlappyBird/Assets/Scripts/PortfolioSiteAccess.cs
+++ Unity/FlappyBird/Assets/Scripts/PortfolioSiteAccess.cs
@@ -35,9 +35,12 @@ namespace MattRGeorge
         #region private
         private const string ROOT_DOMAIN_URL = "http://localhost:56300/";
         private const string SCORES_CONTROLLER = "Scores/";
-        private const string SAVE_SCORE_URL = "SaveScore";
-        private const string GET_SCORES_URL = "GetScores";
-        private const string GET_SCORE_TYPES_URL = "GetScoreTypes";
+        private const string SAVE_SCORE_CONTROLLER = "SaveScore";
+        private const string GET_SCORES_ACTION = "GetScores";
+        private const string GET_USER_SCORES_ACTION = "GetUserScores";
+        private const string GET_TOP_SCORES_ACTION = "GetTopScores";
+        private const string GET_USER_TOP_SCORES_ACTION = "GetUserTopScores";
+        private const string GET_SCORE_TYPES_ACTION = "GetScoreTypes";
 
         private List<string> supportedScoreTypes = new List<string>();
         #endregion
@@ -60,17 +63,82 @@ namespace MattRGeorge
                 return;
             }
 
-            ScoreData data = new ScoreData()
+            ScoreValue data = new ScoreValue()
             {
-                username = user,
+                playerName = user,
                 scoreType = type,
-                amount = score,
+                scoreAmount = score.ToString(),
             };
             StartCoroutine("UploadScore", data);
         }
-        public void StartGettingScores(string user = null)
-        {
 
+        /// <summary>
+        /// Start getting all scores for this game.
+        /// </summary>
+        /// <param name="callbackMethod">The callback method that will be called to send the resulting scores.</param>
+        public void StartGettingScores(Func<List<ScoreValue>, bool> callbackMethod)
+        {
+            if (callbackMethod == null) return;
+
+            GetScoreData data = new GetScoreData()
+            {
+                URLAction = GET_SCORES_ACTION,
+                OnGotScores = callbackMethod,
+            };
+            StartCoroutine("GetScores", data);
+        }
+        /// <summary>
+        /// Start getting all scores for this game for the given user.
+        /// </summary>
+        /// <param name="user">The user that the desired scores belong to.</param>
+        /// <param name="callbackMethod">The callback method that will be called to send the resulting scores.</param>
+        public void StartGettingScores(string user, Func<List<ScoreValue>, bool> callbackMethod)
+        {
+            if (user == null || user == "" || callbackMethod == null) return;
+
+            GetScoreData data = new GetScoreData()
+            {
+                URLAction = GET_USER_SCORES_ACTION,
+                User = user,
+                OnGotScores = callbackMethod,
+            };
+            StartCoroutine("GetScores", data);
+        }
+        /// <summary>
+        /// Start getting the top x scores for this game.
+        /// </summary>
+        /// <param name="topX">The amount of scores to get.</param>
+        /// <param name="callbackMethod">The callback method that will be called to send the resulting scores.</param>
+        public void StartGettingTopScores(int topX, Func<List<ScoreValue>, bool> callbackMethod)
+        {
+            if (topX <= 0 || callbackMethod == null) return;
+
+            GetScoreData data = new GetScoreData()
+            {
+                URLAction = GET_TOP_SCORES_ACTION,
+                TopX = topX,
+                OnGotScores = callbackMethod,
+            };
+            StartCoroutine("GetScores", data);
+        }
+        /// <summary>
+        /// Start getting the top x scores for this game for the given user.
+        /// </summary>
+        /// <param name="user">The user that the desired scores belong to.</param>
+        /// <param name="topX">The amount of scores to get.</param>
+        /// <param name="callbackMethod">The callback method that will be called to send the resulting scores.</param>
+        public void StartGettingTopScores(string user, int topX, Func<List<ScoreValue>, bool> callbackMethod)
+        {
+            if (user == null || user == "" || topX <= 0 || callbackMethod == null) return;
+
+            GetScoreData data = new GetScoreData()
+            {
+                URLAction = GET_USER_TOP_SCORES_ACTION,
+                User = user,
+                TopX = topX,
+                OnGotScores = callbackMethod,
+            };
+            StartCoroutine("GetScores", data);
         }
         #endregion
 
@@ -80,7 +148,7 @@ namespace MattRGeorge
         /// </summary>
         private IEnumerator GetSupportedScoreTypes()
         {
-            using (UnityWebRequest www = UnityWebRequest.Get(ROOT_DOMAIN_URL + SCORES_CONTROLLER + GET_SCORE_TYPES_URL))
+            using (UnityWebRequest www = UnityWebRequest.Get(ROOT_DOMAIN_URL + SCORES_CONTROLLER + GET_SCORE_TYPES_ACTION))
             {
                 //www.certificateHandler = new AcceptAllSelfSignedCerts();
                 PrintDebugMsg_PortfolioSiteAccess("Getting score types...");
@@ -109,29 +177,93 @@ namespace MattRGeorge
         /// Uploads the given score to the site.
         /// </summary>
         /// <param name="data">The score information to be uploaded.</param>
-        private IEnumerator UploadScore(ScoreData data)
+        private IEnumerator UploadScore(ScoreValue data)
         {
             List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
             formData.Add(new MultipartFormDataSection("projectID", serverGameID.ToString()));
-            formData.Add(new MultipartFormDataSection("playerName", data.username));
+            formData.Add(new MultipartFormDataSection("playerName", data.playerName));
             formData.Add(new MultipartFormDataSection("scoreType", data.scoreType));
-            formData.Add(new MultipartFormDataSection("score", data.amount.ToString()));
+            formData.Add(new MultipartFormDataSection("score", data.scoreAmount.ToString()));
 
-            using (UnityWebRequest www = UnityWebRequest.Post(ROOT_DOMAIN_URL + SCORES_CONTROLLER + SAVE_SCORE_URL, formData))
+            using (UnityWebRequest www = UnityWebRequest.Post(ROOT_DOMAIN_URL + SCORES_CONTROLLER + SAVE_SCORE_CONTROLLER, formData))
             {
                 //www.certificateHandler = new AcceptAllSelfSignedCerts();
-                PrintDebugMsg_PortfolioSiteAccess("Uploading score...\n\tUsername: " + data.username + "\n\tScore Type: " + data.scoreType + "\n\tScore: " + data.amount);
+                PrintDebugMsg_PortfolioSiteAccess("Uploading score...\n\tUsername: " + data.playerName + "\n\tScore Type: " + data.scoreType + "\n\tScore: " + data.scoreAmount);
                 yield return www.SendWebRequest();
 
                 if (www.isNetworkError || www.isHttpError) PrintErrorDebugMsg_PortfolioSiteAccess("Error while uploading score: " + www.error + "\n\tURL: " + www.url);
                 else PrintDebugMsg_PortfolioSiteAccess("Uploaded score!");
             }
         }
-        /*private IEnumerator GetScores(string user = null)
-        {
 
-        }*/
+        /// <summary>
+        /// Gets the scores using the data given. Uses a provided callback method to send results.
+        /// </summary>
+        /// <param name="data">The data to be used when getting results.</param>
+        private IEnumerator GetScores(GetScoreData data)
+        {
+            if ((data != null || data.OnGotScores != null) && (data.URLAction != null || data.URLAction != ""))
+            {
+                List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+                formData.Add(new MultipartFormDataSection("projectID", serverGameID.ToString()));
+                if (data.User != null && data.User != "") formData.Add(new MultipartFormDataSection("username", data.User));
+                if (data.TopX > 0) formData.Add(new MultipartFormDataSection("topX", data.TopX.ToString()));
+
+                using (UnityWebRequest www = UnityWebRequest.Post(ROOT_DOMAIN_URL + SCORES_CONTROLLER + data.URLAction, formData))
+                {
+                    //www.certificateHandler = new AcceptAllSelfSignedCerts();
+                    PrintDebugMsg_PortfolioSiteAccess("Getting" + ((data.TopX > 0) ? " Top " + data.TopX : "") + " scores" + ((data.User != null && data.User != "") ? " for " + data.User : "") + "...");
+                    yield return www.SendWebRequest();
+
+                    if (www.isNetworkError || www.isHttpError) PrintErrorDebugMsg_PortfolioSiteAccess("Error while getting scores: " + www.error + "\n\tURL: " + www.url);
+                    else
+                    {
+                        PrintDebugMsg_PortfolioSiteAccess("Received scores! JSON:\n" + www.downloadHandler.text);
+
+                        List<string> extractedJSONs = ExtractJSONFromArray(www.downloadHandler.text);
+                        List<ScoreValue> scores = new List<ScoreValue>();
+                        foreach (string json in extractedJSONs) scores.Add(JsonUtility.FromJson<ScoreValue>(json));
+                        if(!data.OnGotScores(scores)) PrintWarningDebugMsg_PortfolioSiteAccess("OnGetScores callback failed for some reason!");
+                    }
+                }
+            }
+        }
         
+        /// <summary>
+        /// Extracts each object within an array of a JSON string.
+        /// </summary>
+        /// <param name="jsonArrayText">The array of objects in JSON format.</param>
+        /// <returns>A list of JSON objects in their JSON format.</returns>
+        private List<string> ExtractJSONFromArray(string jsonArrayText)
+        {
+            List<string> parsedArray = new List<string>();
+            bool read = false;
+            string currIndex = "";
+            int currBracketCount = 0;
+            foreach (char c in jsonArrayText)
+            {
+                if(c == '{')
+                {
+                    currBracketCount++;
+                    currIndex += c;
+                    read = true;
+                }
+                else if(c == '}' && read)
+                {
+                    currBracketCount--;
+                    currIndex += c;
+                    if (currBracketCount == 0)
+                    {
+                        parsedArray.Add(currIndex);
+                        currIndex = "";
+                        read = false;
+                    }
+                }
+                else if(read) currIndex += c;
+            }
+
+            return parsedArray;
+        }
         /// <summary>
         /// Parses a section of a JSON object that represents one array of strings.
         /// </summary>
@@ -203,13 +335,30 @@ namespace MattRGeorge
         }
         #endregion
         #endregion
-
-        private class ScoreData
+        
+        /// <summary>
+        /// The options that are passed the single parameter IEnumerators getting scores from server.
+        /// </summary>
+        private class GetScoreData
         {
-            public string username = "";
-            public string scoreType = "";
-            public int amount = 0;
+            public string URLAction { get; set; }
+            public string User { get; set; }
+            public int TopX { get; set; }
+            public Func<List<ScoreValue>, bool> OnGotScores { get; set; }
         }
+    }
+
+    /// <summary>
+    /// The values of a score object from the server.
+    /// </summary>
+    [Serializable]
+    public class ScoreValue
+    {
+        public int id = 0;
+        public int projectID = 0;
+        public string playerName = "";
+        public string scoreType = "";
+        public string scoreAmount = "";
     }
 
     public class AcceptAllSelfSignedCerts : CertificateHandler
--- Unity/FlappyBird/Assets/Scripts/Score.cs
+++ Unity/FlappyBird/Assets/Scripts/Score.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using MattRGeorge;
 
 namespace Assets.Scripts
@@ -6,6 +7,7 @@ namespace Assets.Scripts
     public class Score : MonoBehaviour
     {
         public PortfolioSiteAccess siteAccess = null;
+        public bool isDebug = false;
 
         private int amount = 0;
 
@@ -25,6 +27,34 @@ namespace Assets.Scripts
         public void StartUploadScore(string username)
         {
             siteAccess.StartUploadScore(username, "Points", amount);
+        }
+
+        public bool OnGotScores(List<ScoreValue> scores)
+        {
+            if (scores == null) return false;
+
+            if (scores.Count == 0) Debug.Log("No scores!");
+            else
+            {
+                string debugStr = "Scores received:";
+                foreach (ScoreValue score in scores)
+                {
+                    debugStr += "\n\t- " + score.playerName + ", " + score.scoreAmount + ", " + score.scoreType;
+                }
+                Debug.Log(debugStr);
+            }
+
+            return true;
+        }
+        public void Start()
+        {
+            if (isDebug)
+            {
+                siteAccess.StartGettingScores(OnGotScores);
+                siteAccess.StartGettingScores("Bat", OnGotScores);
+                siteAccess.StartGettingTopScores(10, OnGotScores);
+                siteAccess.StartGettingTopScores("Matt", 10, OnGotScores);
+            }
         }
     }
 }
